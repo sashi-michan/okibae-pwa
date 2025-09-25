9df69acd4f9e5eecef2fc9242fdbb6872aebac95
sashi-michan <4141sashi.mi@gmail.com>
2025-09-25 18:35:23 +0900

    revert: Restore PWA configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-// PWA一時無効化 - デバッグ用
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development',
-// });
-// module.exports = withPWA({ reactStrictMode: true });
-
-module.exports = {
-  reactStrictMode: true
-};
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+module.exports = withPWA({ reactStrictMode: true });
