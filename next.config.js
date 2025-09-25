b45508514945f6452b87940d78722655aa5cf971
sashi-michan <4141sashi.mi@gmail.com>
2025-09-25 12:28:47 +0900

    debug: Disable PWA to fix routing issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-module.exports = withPWA({ reactStrictMode: true });
+// PWA一時無効化 - デバッグ用
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
+// });
+// module.exports = withPWA({ reactStrictMode: true });
+
+module.exports = {
+  reactStrictMode: true
+};
